cdf34241f8bf4f7dfb135866b06d394f931e2dcd
fraserdscott <fraserdscott@gmail.com>
2019-10-27 01:05:47 +0300

    Merge branch 'master' of https://github.com/fraserdscott/hive_declarator
    # Conflicts:
    #	polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 import plotly.graph_objs as go
@@ -39,18 +38,18 @@ def count(year, counting_function):
     declarations = get_declarations(year)
 
     counts = {}
-    for decl in declarations:
-        personal_info = decl['main']['person']
+    for declaration in declarations:
+        personal_info = declaration['main']['person']
         person_id = personal_info['id']
         person_name = personal_info['name']
         if person_id not in counts:
             counts[person_id] = {'name': person_name, 'count': 0}
-        counts[person_id]['count'] = counting_function(decl)
+        counts[person_id]['count'] = counting_function(declaration)
     return counts
 
 
 def count_incomes(declaration):
-    return sum(income["size"] for income in declaration["incomes"])
+    return int(sum(income["size"] for income in declaration["incomes"]))  # convert to int because who cares about kopeks
 
 
 def count_vehicles(declaration):
@@ -61,45 +60,40 @@ def count_properties(declaration):
     return len(declaration["real_estates"])
 
 
+def graph_div(counts, layout_title_text):
+    fig = go.Figure(
+        data=[go.Bar(x=[value["name"] for value in counts],
+                     y=[value["count"] for value in counts])],
+        layout_title_text=layout_title_text
+    )
+    div = opy.plot(fig, auto_open=False, output_type='div')
+    return div
+
+
+def rankings_context(counts, title):
+    sorted_counts = sorted(counts.values(), key=lambda v: v['count'], reverse=True)
+
+    div = graph_div(sorted_counts[:10], title)
+
+    return {'rankings': sorted_counts, 'graph': div}
+
+
 def cars(request):
     year = 2018
     counts = count(year, count_vehicles)
-    zesty = sorted(counts.values(), key=lambda v: v['count'], reverse=True)
 
-    context = {'rankings': zesty}
-    return render(request, 'polls/display_rankings.html', context)
+    return render(request, 'polls/display_rankings.html', rankings_context(counts, "Top 10 officials for vehicle ownership"))
 
 
 def incomes(request):
     year = 2018
     counts = count(year, count_incomes)
-    zesty = sorted(counts.values(), key=lambda v: v['count'], reverse=True)
 
-    for d in zesty:
-        d["count"] = int(d["count"])
-
-    div = generate_div(zesty)
-
-    context = {'rankings': zesty, 'graph': div}
-    return render(request, 'polls/display_rankings.html', context)
+    return render(request, 'polls/display_rankings.html', rankings_context(counts, "Top 10 officials for income"))
 
 
 def properties(request):
     year = 2018
     counts = count(year, count_properties)
-    zesty = sorted(counts.values(), key=lambda v: v['count'], reverse=True)
 
-    div = generate_div(zesty)
-
-    context = {'rankings': zesty, 'graph': div}
-    return render(request, 'polls/display_rankings.html', context)
-
-
-def generate_div(zesty):
-    fig = go.Figure(
-        data=[go.Bar(x=[value["name"] for value in zesty],
-                     y=[value["count"] for value in zesty])],
-        layout_title_text="Something per person idk"
-    )
-    div = opy.plot(fig, auto_open=False, output_type='div')
-    return div
+    return render(request, 'polls/display_rankings.html', rankings_context(counts, "Top 10 officials for property ownership"))
